c7eff5dc8718eb837925c0154c9485fea241fe1f
Jennifer Greenberg <jennifergreenberg35@gmail.com>
2020-05-07 18:31:54 -0500

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,9 @@ import click
 from flask import current_app
 from flask import g
 from flask.cli import with_appcontext
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_db():
@@ -69,3 +72,41 @@ def remove(tableName, columnName, item):
     dbCommand = "DELETE FROM " + tableName + " WHERE " + columnName +"=(?)"
     db.execute(dbCommand, (item,))
     db.commit()
+    
+    
+labels = [arguments]
+men_means = [values]
+
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, men_means, width, label='Men')
+rects2 = ax.bar(x + width/2, women_means, width, label='Women')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+autolabel(rects1)
+autolabel(rects2)
+
+fig.tight_layout()
+
+plt.show()
