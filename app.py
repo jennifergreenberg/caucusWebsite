9adc98d2413afb341e69e662e20bb796ca663151
David <kittenthief3@gmail.com>
2020-05-11 20:25:32 -0400

    dwa

--- app.py
+++ app.py
@@ -5,6 +5,9 @@
 ###########################################################################################################################################
 
 from flask import Flask, render_template, url_for, g, redirect, request, flash, session, Blueprint
+from werkzeug.utils import secure_filename
+
+from cand import Candidate
 #import toastr as toastr
 app = Flask(__name__)
 
@@ -21,7 +24,9 @@ jinja2_env = ninja.Environment(loader=ninja.FileSystemLoader(template_dir))
 app.config.from_mapping(
     SECRET_KEY="dev",
     DATABASE=os.path.join(app.instance_path, "myData.sqlite"),
+    UPLOAD_FOLDER = os.path.join(app.instance_path, 'UPLOAD_FOLDER')
 )
+print(app.config['UPLOAD_FOLDER'])
 from db import init_app, get_db, insert, remove, init_app
 from dataVisualBuilder import createGraph
 init_app(app) # initilize the database
@@ -56,6 +61,7 @@ def home():
     return render_template('home.html')
 
 
+
 # Settings Page
 @app.route('/settings', methods=("GET", "POST"))
 @login_required
@@ -65,10 +71,18 @@ def settings():
         if "insert" in request.form:
             name = request.form["candName"]
             bio = request.form["candBio"]
-            image = request.form["filename"]
+            tempimage = request.files["filename"]
+            if tempimage.filename == '':
+                return render_template('index.html', alert="settings")
+
+            filename = secure_filename(tempimage.filename)
+            tempimage.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("\n\n%s\n\n" % image)
+
             error = None
 
-            if not name or not bio or not image:
+            if not name or not bio:
                 error = "Missing Information"
 
             if error is not None:
@@ -105,6 +119,33 @@ def settings():
 # Count Page
 @app.route('/count', methods=("GET", "POST"))
 def count():
+    global Candidates
+    Candidates = []
+	# get candidate names
+    db = get_db()
+    cursor = db.cursor()
+	
+    # get data from candidates table
+    names = []
+    bios = []
+    images = []
+    for row in cursor.execute('SELECT * FROM candidate'):
+        canName = row['name']
+        canImage = row['img']
+        canBio = row['bio']
+        names.append(canName)
+        images.append(canImage)
+        bios.append(canBio)
+	
+    for i in range(0, len(names)):
+        # imageURL = "https://i.imgur.com/yXvE8B1.png"
+        # imageURL = url_for('static', filename=(images[i]))
+        imageURL = images[i]# [40:]
+        # imageURL = imageURL.replace("\\", "/")
+        print(imageURL)
+        cand = Candidate(names[i], bios[i], imageURL)
+        Candidates.append(cand)
+        
     if request.method == "POST":
         db = get_db()
         numOfVotes = request.form['numVotes']
@@ -112,13 +153,38 @@ def count():
         db.execute(
                 "UPDATE candidate WHERE name=(?) SET numVotes=(?)", (name, numOfVotes),
             )
-    return render_template('votes.html')
+    return render_template('votes.html', Candidates=Candidates)
 
 
 # Data Page
 @app.route('/data')
 @login_required
 def data():
+    global Candidates
+    Candidates = []
+	# get candidate names
+    db = get_db()
+    cursor = db.cursor()
+	
+    # get data from candidates table
+    names = []
+    bios = []
+    images = []
+    for row in cursor.execute('SELECT * FROM candidate'):
+        canName = row['name']
+        canImage = row['img']
+        canBio = row['bio']
+        names.append(canName)
+        images.append(canImage)
+        bios.append(canBio)
+	
+    for i in range(0, len(names)):
+        imageURL = images[i]# [40:]
+        print(imageURL)
+        cand = Candidate(names[i], bios[i], imageURL)
+        Candidates.append(cand)
+	
+    # graph
     for filename in os.listdir('static/images'):
         if filename.startswith('graph'):
             os.remove('static/images/' + filename)
---
+++ cand.py
@@ -0,0 +1,5 @@
+class Candidate:
+    def __init__(self, name, bio, pictureURL):
+        self.name = name
+        self.bio = bio
+        self.picture = pictureURL
